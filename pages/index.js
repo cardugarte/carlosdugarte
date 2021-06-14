cd7f992d9740c09714eb28b2fecd988bd5b895ce
cardugarte <contacto@carlosdugarte.com>
2021-06-13 22:13:09 -0300

    Texto de info renovación para el home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hola mundo con Next Js</h1>
+      <h1>En renovación</h1>
+      <p>Hola, en estos momentos estoy construyendo un nuevo sitio con tecnologías más nuevas. Pronto estará disponible</p>
+      <p>Mientras tanto te invito a visitar mi <a href="https://github.com/cardugarte/">repositorio de Github aquí</a></p>
+      <h2>Contáctame</h2>
+      <p>Si prefieres también puedes comunicarte a través de mi email soycarlosdugarte@gmail.com</p>
     </div>
   )
 }
