ac0b0cc86e811093bd8a52602b6c174fee17286d
cardugarte <contacto@carlosdugarte.com>
2021-06-14 20:02:04 -0300

    Modificación de la página de construcción de nueva web

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Image 
-      src="/images/gatoemoji.png" 
-      alt="En construcción"
-      width={300}
-      height={300}
-      />
       <h1>En renovación</h1>
       <p>Hola, en estos momentos estoy construyendo un nuevo sitio con tecnologías más nuevas. Pronto estará disponible</p>
-      <p>Mientras tanto te invito a visitar</p>
-      <button><a target="_blank" href="https://github.com/cardugarte/">repositorio de Github aquí</a></button>
-
-      <h2>Contáctame</h2>
-      <p>Si prefieres también puedes comunicarte a través de mi email</p>
-      <button><a target="_blank" href="mailto:soycarlosdugarte@gmail.com">soycarlosdugarte@gmail.com</a></button>
+        <article>
+        <Image 
+      src="/images/gatoemoji.png" 
+      alt="En construcción"
+      width={350}
+      height={350}
+      />
+        </article>
+        <article className="buttons">
+        <button><a target="_blank" href="mailto:soycarlosdugarte@gmail.com">soycarlosdugarte@gmail.com</a></button>
+        <a target="_blank" href="https://github.com/cardugarte/"><button>repositorio de Github</button></a>
+        </article>
     </div>
   )
 }
