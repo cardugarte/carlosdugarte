1de89db10ff8c1975fbe4b1ecc8c7dca0046b09e
cardugarte <contacto@carlosdugarte.com>
2021-06-14 10:53:47 -0300

    Mejoras en la página que indica la construcción del proyecto

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
+      <Image 
+      src="/images/gatoemoji.png" 
+      alt="En construcción"
+      width={300}
+      height={300}
+      />
       <h1>En renovación</h1>
       <p>Hola, en estos momentos estoy construyendo un nuevo sitio con tecnologías más nuevas. Pronto estará disponible</p>
-      <p>Mientras tanto te invito a visitar mi <a href="https://github.com/cardugarte/">repositorio de Github aquí</a></p>
+      <p>Mientras tanto te invito a visitar</p>
+      <button><a target="_blank" href="https://github.com/cardugarte/">repositorio de Github aquí</a></button>
+
       <h2>Contáctame</h2>
-      <p>Si prefieres también puedes comunicarte a través de mi email soycarlosdugarte@gmail.com</p>
+      <p>Si prefieres también puedes comunicarte a través de mi email</p>
+      <button><a target="_blank" href="mailto:soycarlosdugarte@gmail.com">soycarlosdugarte@gmail.com</a></button>
     </div>
   )
 }
